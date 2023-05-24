38d910a4f0d0136a57d85683fe19b0aa7517e6a2
JoyDanielp <91877231+JoyDanielp@users.noreply.github.com>
2023-05-24 16:14:28 +0530

    Add files via upload
    Details of the MYSQL Joins

---
+++ JOINS SQL.sql
@@ -0,0 +1,7 @@
+Join Type	               Use Case
+Natural Join	           Joins based on common columns
+Inner Join	               Joins based on a given condition
+Left Join	               All rows from left table & matched rows from right table
+Right Join	               All rows from right table & matched rows from left table
+Full Join	               All rows from both the tables
+Cross Join	               All possible combinations
